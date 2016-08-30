ba8376cebc3e91cbd490b34221c41112364a4183
Robert Furr <rfurr@carelogistics.com>
2016-08-30 17:40:45 -0400

    changing the ports and title

--- tools/distServer.js
+++ tools/distServer.js
@@ -11,9 +11,9 @@ console.log(chalkProcessing('Opening production build...'));
 
 // Run Browsersync
 browserSync({
-  port: 3000,
+  port: 3005,
   ui: {
-    port: 3001
+    port: 3006
   },
   server: {
     baseDir: 'dist'
