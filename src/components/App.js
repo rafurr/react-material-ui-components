615ac95c5749158a8778abd6a58a123af7642ca2
Robert Furr <rafurr@gmail.com>
2016-09-05 23:33:31 -0400

    general refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import {Link, IndexLink} from 'react-router';
 const App = (props) => {
   return (
     <div>
-      <IndexLink to="/react-material-ui-components">Home</IndexLink>
+      <IndexLink to="/react-material-ui-components/">Home</IndexLink>
       {' | '}
       <Link to="/react-material-ui-components/demo">Demo</Link>
       {' | '}
